73efffe6e0cf9a5c40a99532e4942ba510e4f482
Flo Hacker <flori@bin.org.in>
2017-03-25 12:36:43 +0100

    Working on completing a build run. Removing obsolete Android stuff, ril linker error fixed, preparing fstab for Ubuntu Touch

--- ril-wrapper/Android.mk
+++ ril-wrapper/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= ril-wrapper.c
-LOCAL_SHARED_LIBRARIES := liblog libbinder
+LOCAL_SHARED_LIBRARIES := liblog libbinder libdl
 LOCAL_MODULE:= ril-wrapper
 
 include $(BUILD_SHARED_LIBRARY)
